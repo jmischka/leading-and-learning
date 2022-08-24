52aadd13cc60f717f84ae421a14a9ea404f045a5
Jason Mischka <jasonmischka@Jasons-MBP.home>
2022-08-24 07:23:24 -0400

    updated testimonial interval time

--- components/testimonial.js
+++ components/testimonial.js
@@ -207,7 +207,7 @@ export default function Testimonial({serviceTitle, testimonialData}) {
 
     useEffect(() => {
         if (slideShow) {
-            const interval = setInterval(slideRotater, 6000);
+            const interval = setInterval(slideRotater, 7000);
             return () => clearInterval(interval);
         }
     }, [slideShow]);
