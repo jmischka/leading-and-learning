8ec81dd7724f4cc45c8370778a2b13b12b535ebe
Jason Mischka <jasonmischka@Jasons-MBP.fios-router.home>
2022-10-29 16:29:44 -0400

    updated button hover colors

--- components/opportunity-brief.js
+++ components/opportunity-brief.js
@@ -23,22 +23,49 @@ const Opportunity = styled.li`
         line-height: 1.3;
     }
 
-    button {
-        margin: 16px 0 0;
-        padding: 9px 12px;
-        border: 1px solid;
-        background-color: transparent;
-        cursor: pointer;
-        transition-duration: .5s;
-        &:hover {
-            background-color: ${COLORS.primaryBlue};
-            border: none;
-            color: white;
-        }
-    }
-
     @media screen and (max-width: 900px) {
         width: 100%;
+    }
+`;
+
+const Button = styled.button`
+    margin: 16px 0 0;
+    font-family: 'AvenirNextLTW02', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    padding: 9px 12px;
+    background-color: transparent;
+    color: #333333;
+    border: ${props => props.category === 'shepherding' 
+        ? `1px solid ${COLORS.shepherdPrimary}` 
+        : props.category === 'tutoring'
+        ? `1px solid ${COLORS.tutorPrimary}`
+        : props.category === 'testprep'
+        ? `1px solid ${COLORS.testPrimary}`
+        : props.category === 'educationalconsulting'
+        ? `1px solid ${COLORS.educationPrimary}`
+        : `1px solid ${COLORS.primaryBlue}`};
+    cursor: pointer;
+    transition-duration: .3s;
+    &:hover {
+        background-color: ${props => props.category === 'shepherding' 
+            ? `${COLORS.shepherdPrimary}`
+            :  props.category === 'tutoring'
+            ? `${COLORS.tutorPrimary}`
+            :  props.category === 'testprep'
+            ? `${COLORS.testPrimary}`
+            :  props.category === 'educationalconsulting'
+            ? `${COLORS.educationPrimary}`
+            : `${COLORS.primaryBlue}`};
+        border: ${props => props.category === 'shepherding' 
+            ? `1px solid ${COLORS.shepherdPrimary}` 
+            : props.category === 'tutoring'
+            ? `1px solid ${COLORS.tutorPrimary}`
+            : props.category === 'testprep'
+            ? `1px solid ${COLORS.testPrimary}`
+            : props.category === 'educationalconsulting'
+            ? `1px solid ${COLORS.educationPrimary}`
+            : `1px solid ${COLORS.primaryBlue}`};
+        color: white;
     }
 `;
 
@@ -49,6 +76,13 @@ const careerColor = (category) => {
     if (category === 'shepherding') {
         return `${COLORS.shepherdPrimary}`;
     }
+    if (category === 'testprep') {
+        return `${COLORS.testPrimary}`;
+    }
+    if (category === 'educationalconsulting') {
+        return `${COLORS.educationPrimary}`;
+    }
+    return `${COLORS.primaryBlue}`;
 }
 
 function OpportunityBrief({category, title, description, dataTag, handleOpportunityClick}) {
@@ -57,12 +91,12 @@ function OpportunityBrief({category, title, description, dataTag, handleOpportun
             <span className='jobColorDash' style={{backgroundColor: careerColor(category)}} />
             <h3 style={{color: careerColor(category)}}>{title}</h3>
             <p>{description[0].children[0].text}</p>
-            <button 
+            <Button 
+                category={category}
                 data-opportunity={dataTag} 
-                style={{borderColor: careerColor(category)}} 
                 onClick={handleOpportunityClick}>
                     Learn More
-            </button>
+            </Button>
         </Opportunity>
     )
 }
